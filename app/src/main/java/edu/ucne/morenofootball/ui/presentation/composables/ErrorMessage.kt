96ce18e7f74d4ed2b1ea651948daea27714927cd
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-11-22 09:26:56 -0400

    feat: Create `ErrorMessage` composable
    Create a new composable function named `ErrorMessage` to display an error message.
    This composable uses `AnimatedVisibility` to show a styled `Text` only when the provided error message string is not null or blank.

---
+++ app/src/main/java/edu/ucne/morenofootball/ui/presentation/composables/ErrorMessage.kt
@@ -0,0 +1,24 @@
+package edu.ucne.morenofootball.ui.presentation.composables
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ErrorMessage(
+    errorMessage: String?,
+) {
+    AnimatedVisibility(visible = !errorMessage.isNullOrBlank()) {
+        Text(
+            text = errorMessage ?: "",
+            color = MaterialTheme.colorScheme.error,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier
+                .padding(top = 2.dp)
+        )
+    }
+}

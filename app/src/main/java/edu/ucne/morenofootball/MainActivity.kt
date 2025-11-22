d8057d49636dcc69bb4d6d9b0227806213aa854a
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-11-22 09:57:45 -0400

    feat: Define navigation screens
    Create a sealed class `Screen` to define the application's navigation destinations using Kotlin's serialization library.
    
    This class includes `Login`, `Home`, and `Register` as serializable objects, representing the different screens in the app.

--- app/src/main/java/edu/ucne/morenofootball/MainActivity.kt
+++ app/src/main/java/edu/ucne/morenofootball/MainActivity.kt
@@ -4,14 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.morenofootball.ui.presentation.navigation.MorenoFootBallNavHost
 import edu.ucne.morenofootball.ui.theme.MorenoFootballTheme
 
 @AndroidEntryPoint
@@ -21,9 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MorenoFootballTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    innerPadding
-                }
+                MorenoFootBallNavHost()
             }
         }
     }

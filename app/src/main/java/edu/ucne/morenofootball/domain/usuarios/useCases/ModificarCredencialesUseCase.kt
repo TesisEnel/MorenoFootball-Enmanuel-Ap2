ba086f324478d371502b5a1d1c6cb4f795e0c31d
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-11-22 01:34:10 -0400

    refactor: Improve credential modification handling
    Enhance the `ModificarCredencialesUseCase` to explicitly handle `Success`, `Error`, and `Loading` states from the repository.
    This provides more robust state management by propagating the specific `Resource` type instead of directly returning the repository's result.

--- app/src/main/java/edu/ucne/morenofootball/domain/usuarios/useCases/ModificarCredencialesUseCase.kt
+++ app/src/main/java/edu/ucne/morenofootball/domain/usuarios/useCases/ModificarCredencialesUseCase.kt
@@ -8,6 +8,11 @@ import javax.inject.Inject
 class ModificarCredencialesUseCase @Inject constructor(
     private val repo: UsuarioRepository,
 ) {
-    suspend operator fun invoke(credenciales: ModificarCredenciales): Resource<Unit> =
-        repo.modificarCredenciales(credenciales)
+    suspend operator fun invoke(credenciales: ModificarCredenciales): Resource<Unit> {
+        return when(val result = repo.modificarCredenciales(credenciales)){
+            is Resource.Success -> Resource.Success(Unit)
+            is Resource.Error -> Resource.Error(result.message ?: "")
+            is Resource.Loading -> Resource.Loading()
+        }
+    }
 }

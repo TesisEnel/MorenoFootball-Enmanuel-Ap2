66d028f3b3709ebe938dd1d53fe217dc2eab187e
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-11-28 15:58:42 -0400

    feat: Add TarjetaRepositorImpl
    Create the `TarjetaRepositorImpl` class, which injects `TarjetaRemoteDataSource`.
    The implementation details are pending.

---
+++ app/src/main/java/edu/ucne/morenofootball/data/tarjetas/TarjetaRepositorImpl.kt
@@ -0,0 +1,10 @@
+package edu.ucne.morenofootball.data.tarjetas
+
+import edu.ucne.morenofootball.data.tarjetas.remote.TarjetaRemoteDataSource
+import javax.inject.Inject
+
+class TarjetaRepositorImpl @Inject constructor(
+    private val remote: TarjetaRemoteDataSource
+) {
+    // TODO("Not implemented yet.")
+}

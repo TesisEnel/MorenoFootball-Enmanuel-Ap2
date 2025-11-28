4cf46dfbe37bdbc866585a08c365260ba171dfb2
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-11-28 11:01:46 -0400

    feat: Implement user registration and enhance login logic
    Implement the user registration feature, including input validation for username, email, password, and password confirmation.
    Handle registration state (loading, success, error) and navigate to the home screen upon successful registration.
    
    Refactor the login process to include a `rememberUser` option and differentiate between login and registration password validation.
    Add UI event handlers for username changes, password confirmation, toggling "remember user," and switching between login and registration forms.

--- app/src/main/java/edu/ucne/morenofootball/ui/presentation/usuarios/login/LoginViewModel.kt
+++ app/src/main/java/edu/ucne/morenofootball/ui/presentation/usuarios/login/LoginViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import edu.ucne.morenofootball.domain.usuarios.models.Login
+import edu.ucne.morenofootball.domain.usuarios.models.Register
 import edu.ucne.morenofootball.domain.usuarios.useCases.UsuarioUseCases
 import edu.ucne.morenofootball.utils.Resource
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -31,29 +32,132 @@ class LoginViewModel @Inject constructor(
 
     fun onEvent(event: LoginUiEvent) {
         when (event) {
-            is LoginUiEvent.Login -> login(event.email, event.clave)
-            is LoginUiEvent.OnPasswordChange -> _state.update { it.copy(password = event.password, errorPass = null) }
-            is LoginUiEvent.OnEmailChange -> _state.update { it.copy(email = event.email, errorEmail = null) }
+            is LoginUiEvent.Login -> login(event.email, event.password, event.rememberUser)
+            is LoginUiEvent.Register -> register(
+                event.usermame,
+                event.email,
+                event.password,
+                event.confirmPassword
+            )
+
+            is LoginUiEvent.OnPasswordChange -> _state.update {
+                it.copy(
+                    password = event.password,
+                    errorPass = null
+                )
+            }
+
+            is LoginUiEvent.OnEmailChange -> _state.update {
+                it.copy(
+                    email = event.email,
+                    errorEmail = null
+                )
+            }
+
             is LoginUiEvent.OnTogglePasswordVisibility -> _state.update { it.copy(isPasswordVisible = !it.isPasswordVisible) }
+            is LoginUiEvent.OnUsernameChange -> _state.update {
+                it.copy(
+                    username = event.username,
+                    errorUsername = null
+                )
+            }
+
+            is LoginUiEvent.OnConfirmPasswordChange -> _state.update {
+                it.copy(
+                    confirmPassword = event.confirmPassword,
+                    errorConfirmPass = null
+                )
+            }
+
+            LoginUiEvent.OnToggleRememberUser -> _state.update { it.copy(rememberUser = !it.rememberUser) }
+            LoginUiEvent.OnChangeIsUserHaveAnAccount -> _state.update { it.copy(isUserHaveAnAccount = !it.isUserHaveAnAccount) }
+        }
+    }
+
+    private fun register(
+        usermame: String,
+        email: String,
+        password: String,
+        confirmPassword: String,
+    ) {
+        viewModelScope.launch {
+            _state.update { it.copy(isLoading = true) }
+
+            val userValidation =
+                usuarioUseCases.validarLoginRegisterUseCase.validarUsername(usermame)
+            val emailValidation = usuarioUseCases.validarLoginRegisterUseCase.validarEmail(email)
+            val passwordValidation =
+                usuarioUseCases.validarLoginRegisterUseCase.validarPasswordRegister(password)
+            val confirmPasswordValidation =
+                usuarioUseCases.validarLoginRegisterUseCase.validarConfirmPassword(
+                    password,
+                    confirmPassword
+                )
+            if (!emailValidation.esValido || !passwordValidation.esValido) {
+                _state.update {
+                    it.copy(
+                        errorEmail = emailValidation.error,
+                        errorPass = passwordValidation.error,
+                        errorConfirmPass = confirmPasswordValidation.error,
+                        errorUsername = userValidation.error,
+                        isLoading = false
+                    )
+                }
+                return@launch
+            }
+
+            val result = usuarioUseCases.registerUseCase(
+                Register(
+                    email = email,
+                    password = password,
+                    username = usermame,
+                )
+            )
+            when (result) {
+                is Resource.Error -> _state.update {
+                    it.copy(
+                        message = result.message,
+                        isLoading = false
+                    )
+                }
+
+                is Resource.Loading -> _state.update { it.copy(message = "", isLoading = true) }
+                is Resource.Success -> {
+                    Log.d("LoginViewModel", "Info de sesion: ${result.data}")
+                    _navigateToHome.emit(Unit)
+                    _state.update {
+                        it.copy(
+                            message = "¡Registro exitoso!",
+                            isLoading = false,
+                            errorPass = null,
+                            errorEmail = null
+                        )
+                    }
+                }
+            }
         }
     }
 
     private fun getUsuarioLoggeado() {
         viewModelScope.launch {
             val user = usuarioUseCases.getUsuarioLoggeadoUseCase()
-            if(user != null) {
+            if(user != null && user.rememberUser)
                 _navigateToHome.emit(Unit)
-                _state.update { it.copy(isLoading = false) }
-            }
+            _state.update { it.copy(isLoading = false) }
         }
     }
 
-    private fun login(email: String, password: String) {
+    private fun login(
+        email: String,
+        password: String,
+        rememberUser: Boolean,
+    ) {
         viewModelScope.launch {
             _state.update { it.copy(isLoading = true) }
 
             val emailValidation = usuarioUseCases.validarLoginRegisterUseCase.validarEmail(email)
-            val passwordValidation = usuarioUseCases.validarLoginRegisterUseCase.validarPassword(password)
+            val passwordValidation =
+                usuarioUseCases.validarLoginRegisterUseCase.validarPasswordLogin(password)
             if (!emailValidation.esValido || !passwordValidation.esValido) {
                 _state.update {
                     it.copy(
@@ -68,13 +172,20 @@ class LoginViewModel @Inject constructor(
             val result = usuarioUseCases.loginUseCase(
                 Login(
                     email = email,
-                    password = password
+                    password = password,
+                    rememberUser = rememberUser
                 )
             )
             when (result) {
-                is Resource.Error<*> -> _state.update { it.copy(message = result.message, isLoading = false) }
-                is Resource.Loading<*> -> _state.update { it.copy(message = "", isLoading = true) }
-                is Resource.Success<*> -> {
+                is Resource.Error -> _state.update {
+                    it.copy(
+                        message = result.message,
+                        isLoading = false
+                    )
+                }
+
+                is Resource.Loading -> _state.update { it.copy(message = "", isLoading = true) }
+                is Resource.Success -> {
                     Log.d("LoginViewModel", "Info de sesion: ${result.data}")
                     _navigateToHome.emit(Unit)
                     _state.update {

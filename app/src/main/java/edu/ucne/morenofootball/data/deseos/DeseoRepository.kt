406764ff04345d66bae6af26cb4ad24c44686d07
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-11-29 00:57:22 -0400

    chore: Add TODO in DeseoRepository
    Add a `TODO` comment to the `DeseoRepository` class to mark it as not yet implemented.

---
+++ app/src/main/java/edu/ucne/morenofootball/data/deseos/DeseoRepository.kt
@@ -0,0 +1,10 @@
+package edu.ucne.morenofootball.data.deseos
+
+import edu.ucne.morenofootball.data.deseos.remote.DeseoRemoteDataSource
+import javax.inject.Inject
+
+class DeseoRepository @Inject constructor(
+    private val remote: DeseoRemoteDataSource
+) {
+    // TODO("Not implemented yet.")
+}

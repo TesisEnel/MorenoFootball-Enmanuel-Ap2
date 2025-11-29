2e7cfea3740cc0f4a6a9a63e26798f54f5b853a4
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-11-29 01:02:25 -0400

    refactor: Rename ModificarCantidadParams to ActionWithProductFromCardParams
    Rename the data class `ModificarCantidadParams` to `ActionWithProductFromCardParams`.
    This new name more accurately reflects its purpose of handling various actions on a product within the shopping cart, not just modifying the quantity.

--- app/src/main/java/edu/ucne/morenofootball/data/carritos/remote/dto/request/ActionWithProductFromCardParams.kt
+++ app/src/main/java/edu/ucne/morenofootball/data/carritos/remote/dto/request/ActionWithProductFromCardParams.kt
@@ -1,6 +1,6 @@
 package edu.ucne.morenofootball.data.carritos.remote.dto.request
 
-data class ModificarCantidadParams(
+data class ActionWithProductFromCardParams(
     val usuarioId: Int?,
     val productoId: Int,
     val sesionAnonimaId: String? = null

617cc9912ec6269f304cd2c179a1efe0602af694
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-12-07 12:20:42 -0400

    refactor: Update data types in TarjetaRequest
    Change the data types for `bin` from `String` to `Long` and `cvv` from `String` to `Int` to better represent numeric card information.

--- app/src/main/java/edu/ucne/morenofootball/data/tarjetas/remote/dto/request/TarjetaRequest.kt
+++ app/src/main/java/edu/ucne/morenofootball/data/tarjetas/remote/dto/request/TarjetaRequest.kt
@@ -1,9 +1,9 @@
 package edu.ucne.morenofootball.data.tarjetas.remote.dto.request
 
-data class TarjetaRequest(
+data class TarjetaRequest (
     val usuarioId: Int,
-    val bin: String,
-    val cvv: String,
+    val bin: Long,
+    val cvv: Int,
     val nombreTitular: String,
     val fechaVencimiento: String,
 )

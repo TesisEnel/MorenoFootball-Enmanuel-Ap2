ae5dc7594b98cbd632b7c47e05c3a29310e92dc0
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-12-07 12:27:46 -0400

    refactor: Update Tarjeta data types
    Change the data types for `bin` from `String` to `Long` and `cvv` from `String` to `Int` in the `Tarjeta` model.

--- app/src/main/java/edu/ucne/morenofootball/domain/tarjetas/models/Tarjeta.kt
+++ app/src/main/java/edu/ucne/morenofootball/domain/tarjetas/models/Tarjeta.kt
@@ -3,8 +3,8 @@ package edu.ucne.morenofootball.domain.tarjetas.models
 data class Tarjeta(
     val tarjetaId: Int = 0,
     val usuarioId: Int = 0,
-    val bin: String = "",
-    val cvv: String = "",
+    val bin: Long = 0,
+    val cvv: Int = 0,
     val nombreTitular: String = "",
     val fechaVencimiento: String = "",
     val tipoTarjeta: String = "",

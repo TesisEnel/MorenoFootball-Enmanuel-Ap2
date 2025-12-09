2f043710376abb6f8d1b7502f96434a86df24ce2
Enmanuel0723 <enmanuelgomez0728@gmail.com>
2025-12-09 13:35:06 -0400

    feat: Add mappers for Pedido data models
    Create the `PedidoMapper.kt` file to handle conversions between data transfer objects (DTOs) and domain models for pedido-related data.
    
    This includes mappers for:
    - `PedidoResponseDto` to `PedidoResponse`
    - `PedidoDetalleResponseDto` to `PedidoDetalleResponse`
    - `PedidoRequest` to `PedidoRequestDto`
    - `PedidoDetalleRequest` to `PedidoDetalleRequestDto`

---
+++ app/src/main/java/edu/ucne/morenofootball/data/pedidos/PedidoMapper.kt
@@ -0,0 +1,48 @@
+package edu.ucne.morenofootball.data.pedidos
+
+import edu.ucne.morenofootball.data.pedidos.remote.dto.request.PedidoDetalleRequestDto
+import edu.ucne.morenofootball.data.pedidos.remote.dto.request.PedidoRequestDto
+import edu.ucne.morenofootball.data.pedidos.remote.dto.response.PedidoDetalleResponseDto
+import edu.ucne.morenofootball.data.pedidos.remote.dto.response.PedidoResponseDto
+import edu.ucne.morenofootball.domain.pedidos.models.request.PedidoDetalleRequest
+import edu.ucne.morenofootball.domain.pedidos.models.request.PedidoRequest
+import edu.ucne.morenofootball.domain.pedidos.models.response.PedidoDetalleResponse
+import edu.ucne.morenofootball.domain.pedidos.models.response.PedidoResponse
+
+fun PedidoResponseDto.toDomain() = PedidoResponse(
+    pedidoId,
+    usuarioId,
+    tarjetaId,
+    fechaPedido,
+    fechaEnviado,
+    fechaEntrega,
+    estaEntregado,
+    estaEnviado,
+    detalles.map { it.toDomain() }
+)
+
+fun PedidoDetalleResponseDto.toDomain() = PedidoDetalleResponse(
+    detalleId,
+    pedidoId,
+    productoId,
+    cantidad,
+)
+
+fun PedidoRequest.toDto() = PedidoRequestDto(
+    pedidoId,
+    usuarioId,
+    tarjetaId,
+    fechaPedido,
+    fechaEnviado,
+    fechaEntrega,
+    estaEntregado,
+    estaEnviado,
+    detalles.map { it.toDto() }
+)
+
+fun PedidoDetalleRequest.toDto() = PedidoDetalleRequestDto(
+    detalleId,
+    pedidoId,
+    productoId,
+    cantidad,
+)
